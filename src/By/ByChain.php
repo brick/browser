f07d44b101693cefc55f369a205986b6335c1af9
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-03 12:34:00 +0100

    Use strict_types

--- src/By/ByChain.php
+++ src/By/ByChain.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Brick\Browser\By;
 
 use Brick\Browser\By;
--- src/Exception/UnexpectedElementException.php
+++ src/Exception/UnexpectedElementException.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Brick\Browser\Exception;
 
 class UnexpectedElementException extends BrowserException
